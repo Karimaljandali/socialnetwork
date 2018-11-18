53c57b8e4d69781b86ba716c57469bb0f9b8ed93
Karim Aljandali <karimaljandali@gmail.com>
2018-11-18 14:35:01 -0500

    added cookies

--- login.php
+++ login.php
@@ -11,6 +11,24 @@ if (isset($_POST['login'])) {
 		// Check if password is correct
 		if (password_verify($password, DB::query('SELECT password FROM users WHERE username=:username', array('username'=>$username))[0]['password'])){
 			echo "Logged in";
+			// variable is set because second param must be variable.
+			$cstrong = True;
+			// Token is generated with oppenssl_random_pseudo_bytes and then converted from binary to hex
+			$token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
+			// Get user id from table for later reference
+			$user_id = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$username))[0]['id'];
+			// Add token to token table. Encrypt token with sha1 for security
+			DB::query('INSERT INTO login_tokens VALUES (\'\', :token, :user_id)', array(':token'=>sha1($token), ':user_id'=>$user_id));
+
+			// Set cookie in browser
+			// @param 1: Cookie name
+			// @param 2: token id
+			// @param 3: cookie duration (1 week)
+			// @param 4: where is cookie valid (all)
+			// @param 5: domain cookie is valid on
+			// @param 6: domain not valid on SSL
+			// @param 7: http only, javascript cant access cookie
+			setcookie("SNID", $token, time() + 60 * 60 * 24 * 7, '/', NULL, NULL, TRUE);
 		} else{
 			echo "Incorrect password";
 		}
